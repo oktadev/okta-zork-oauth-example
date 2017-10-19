382a3a527699fbd0d948e68f23a7980ac15408e5
Micah Silverman <micah@afitnerd.com>
2017-10-19 12:48:40 -0400

    fixed runapp for exports

--- runapp.sh
+++ runapp.sh
@@ -1,23 +1,7 @@
 #!/usr/bin/env bash
 
-cat<<EOF
-about to run:
-
-java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar \\
-    --okta.client.orgUrl=$OKTA_ORG_URL \\
-    --okta.client.token=$OKTA_API_TOKEN \\
-    --security.oauth2.client.clientId=$OKTA_CLIENT_ID \\
-    --security.oauth2.client.clientSecret=$OKTA_CLIENT_SECRET \\
-    --security.oauth2.client.accessTokenUri=$OKTA_ORG_URL/oauth2/$OKTA_AUTHORIZATION_SERVER_ID/v1/token \\
-    --security.oauth2.client.userAuthorizationUri=$OKTA_ORG_URL/oauth2/$OKTA_AUTHORIZATION_SERVER_ID/v1/authorize \\
-    --security.oauth2.resource.userInfoUri=$OKTA_ORG_URL/oauth2/$OKTA_AUTHORIZATION_SERVER_ID/v1/userinfo \\
-    --security.oauth2.resource.openIdConfigUri=$OKTA_ORG_URL/oauth2/$OKTA_AUTHORIZATION_SERVER_ID/.well-known/openid-configuration \\
-    --security.oauth2.client.clientAuthenticationScheme=form \\
-    --security.oauth2.client.scope="openid profile email" \\
-    --security.oauth2.resource.preferTokenInfo=false
-
-
-EOF
+export OKTA_CLIENT_TOKEN=$OKTA_API_TOKEN
+export OKTA_CLIENT_ORGURL=$OKTA_ORG_URL
 
 java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar \
     --okta.client.orgUrl=$OKTA_ORG_URL \

5c44412ccd7fd3224289f923a26b5f2046da74dc
Micah Silverman <micah@afitnerd.com>
2017-10-19 00:58:57 -0400

    refactored Procfile and added runapp.sh to reduce config settings

--- src/main/java/com/okta/examples/zorkoauth/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/okta/examples/zorkoauth/config/SpringSecurityWebAppConfig.java
@@ -3,7 +3,6 @@ package com.okta.examples.zorkoauth.config;
 
 import com.okta.examples.zorkoauth.jwt.JWTFilter;
 import com.okta.examples.zorkoauth.jwt.TokenProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,8 +13,11 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableOAuth2Sso
 public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    TokenProvider tokenProvider;
+    private TokenProvider tokenProvider;
+
+    public SpringSecurityWebAppConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
